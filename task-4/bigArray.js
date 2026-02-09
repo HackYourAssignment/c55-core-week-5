3e742c6f3ad48ccfdc49b9e3c41c909bc557c8d0
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-02-10 00:24:30 +0100

    fix comments

--- task-4/bigArray.js
+++ task-4/bigArray.js
@@ -10,28 +10,3 @@ function generateBigArray(size) {
 }
 
 export default generateBigArray;
-
-// import { binarySearch } from "./search-experiment.js";
-// import { linearSearch } from "./linear_search_algorithm.js";
-
-// const array_1 = new Array(100).fill(0);
-// array_1[73] = 73;
-
-// const array_2 = new Array(1000).fill(0);
-// array_2[679] = 679;
-
-// const array_3 = new Array(10000).fill(0);
-// array_3[9345] = 9345;
-
-// //first
-// console.time("binary_1");
-// binarySearch([array_1], 73);
-// console.timeEnd("binary_1");
-
-// console.time(linearSearch([array_1], 73));
-
-// console.time(binarySearch([array_2], 679));
-// console.time(linearSearch([array_2], 679));
-
-// console.time(binarySearch([array_3], 9345));
-// console.time(linearSearch([array_3], 9345));
