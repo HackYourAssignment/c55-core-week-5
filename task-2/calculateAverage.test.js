3c06bcac23514af32db9bae3bf912590bab47dea
Halyna Romanyshyn <30292604+halyna1995@users.noreply.github.com>
2026-02-10 00:43:51 +0100

    Refactor tests for calculateAverage function

--- task-2/calculateAverage.test.js
+++ task-2/calculateAverage.test.js
@@ -10,24 +10,43 @@ describe("calculateAverage", () => {
     expect(result).toBe(4);
   });
   test("returns the number itself for a single-item array [5] -> 5", () => {
+    // Arrange
     const numbers = [5];
+    // Act
     const result = calculateAverage(numbers);
+    // Assert
     expect(result).toBe(5);
   });
   test("returns null for an empty array", () => {
-    const result = calculateAverage([]);
+    // Arrange
+    const numbers = [];
+    // Act
+    const result = calculateAverage(numbers);
+    // Assert
     expect(result).toBeNull();
   });
   test("returns null for non-array input (string)", () => {
-    const result = calculateAverage("123");
+    // Arrange
+    const numbers = "123";
+    // Act
+    const result = calculateAverage(numbers);
+    // Assert
     expect(result).toBeNull();
   });
   test("returns null for non-array input (null)", () => {
-    const result = calculateAverage(null);
+    // Arrange
+    const numbers = null;
+    // Act
+    const result = calculateAverage(numbers);
+    // Assert
     expect(result).toBeNull();
   });
   test('returns null if array contains a non-number value [1,2,"3"]', () => {
-    const result = calculateAverage([1, 2, "3"]);
+    // Arrange
+    const numbers = [1, 2, "3"];
+    // Act
+    const result = calculateAverage([numbers);
+    // Assert
     expect(result).toBeNull();
   });
 });
