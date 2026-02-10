34ae67a1e85842bc3ce99de792acf2e982d370b4
baraah <baraah.alshiaani@gmail.com>
2026-02-10 22:26:41 +0100

    first commit done

--- task-1/count-above-threshold.js
+++ task-1/count-above-threshold.js
@@ -1,1 +1,15 @@
 //Your code here
+function countAboveThreshold(numbers, threshold) {
+  let count = 0;
+
+  for (let i=0;i<numbers.length; i++){
+    if(numbers[i] > threshold){
+        count++;
+    }
+  }
+  return count;
+}
+
+console.log(countAboveThreshold([1,5,10,3],4));// returns 2
+console.log(countAboveThreshold([7,8,9],10));// returns 0
+console.log(countAboveThreshold([],5));// returns 0
