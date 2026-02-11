eb310876ae4f759c352b4a886d81ed5a88b7cc11
Mustafa Homsi <your-homsi6313@gmail.com>
2026-02-11 09:39:31 +0100

    t1w5 done

--- task-1/count-above-threshold.js
+++ task-1/count-above-threshold.js
@@ -1,1 +1,12 @@
-//Your code here
+function countAboveThreshold(numbers, threshold) {
+  let count = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > threshold) {
+      count++;
+    }
+  }
+
+  return count;
+}
+module.exports = countAboveThreshold;
