af1020cf2f82eb894f7e6721a648f025de160bbf
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-11 18:59:41 +0100

    does binary search

--- task-4/search-experiment.js
+++ task-4/search-experiment.js
@@ -2,8 +2,22 @@
 // Use generateBigArray to create a big array numbers.
 // Example: generateBigArray(1000000) will create an array of 1 million numbers.
 
-import {generateBigArray} from "./bigArray";
+import { generateBigArray } from "./bigArray";
 
-function binary_search(array,target){
-    
+function binary_search(array, target) {
+  let L = 0;
+  let R = array.length - 1;
+
+  while (L <= R) {
+    const m = L + Math.floor((R - L) / 2);
+
+    if (array[m] < target) {
+      L = m + 1;
+    } else if (array[m] > target) {
+      R = m - 1;
+    } else {
+      return m;
+    }
+  }
+  return "unsuccessful";
 }
