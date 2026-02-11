fa0bb59ca6aff58405a8b10466eccdafa557a52e
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-11 18:51:07 +0100

    corrected file name

--- task-2/calculateAverage.test.js
+++ task-2/calculateAverage.test.js
@@ -1,30 +1,28 @@
-import { describe, test, expect } from 'vitest';
-import { calculateAverage } from './calculateAverage';
+import { describe, test, expect } from "vitest";
+import { calculateAverage } from "./calculateAverage";
 
-describe('calculateAverage checks', () => {
-
-  test('normal numbers → should give 4 for [2, 4, 6]', () => {
+describe("calculateAverage checks", () => {
+  test("normal numbers → should give 4 for [2, 4, 6]", () => {
     expect(calculateAverage([2, 4, 6])).toBe(4);
   });
 
-  test('one number → should give back the same number', () => {
+  test("one number → should give back the same number", () => {
     expect(calculateAverage([5])).toBe(5);
     expect(calculateAverage([0])).toBe(0);
   });
 
-    test('empty list → should give null', () => {
+  test("empty array → should give null", () => {
     expect(calculateAverage([])).toBe(null);
   });
 
-   test('not a list at all → should give null', () => {
+  test("has a non array input → should give null", () => {
     expect(calculateAverage(null)).toBe(null);
     expect(calculateAverage("hello")).toBe(null);
     expect(calculateAverage(100)).toBe(null);
   });
 
-  test('list with non number value → should give null', () => {
+  test("list with non number value → should give null", () => {
     expect(calculateAverage([1, 2, "hello"])).toBe(null);
     expect(calculateAverage([1, true, 3])).toBe(null);
   });
-
 });
