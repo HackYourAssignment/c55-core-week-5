2efa73e4d31f652af521b35a94079197b5e8b292
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-11 18:50:32 +0100

    imported generateBigArray

--- task-4/search-experiment.js
+++ task-4/search-experiment.js
@@ -1,3 +1,9 @@
 // Write your code here
 // Use generateBigArray to create a big array numbers.
 // Example: generateBigArray(1000000) will create an array of 1 million numbers.
+
+import {generateBigArray} from "./bigArray";
+
+function binary_search(array,target){
+    
+}
