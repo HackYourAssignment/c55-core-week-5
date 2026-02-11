5daa6d6a0ce8768479a1a1378e056a9d48d18b96
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-11 10:58:44 +0100

    empty list

--- task-2/calculateAverageTest.js
+++ task-2/calculateAverageTest.js
@@ -1,8 +1,17 @@
-import { describe, test, expect } from "vitest";
-import { calculateAverage } from "./calculateAverage";
+import { describe, test, expect } from 'vitest';
+import { calculateAverage } from './calculateAverage';
 
 describe('calculateAverage checks', () => {
 
   test('normal numbers → should give 4 for [2, 4, 6]', () => {
     expect(calculateAverage([2, 4, 6])).toBe(4);
+  });
+
+  test('one number → should give back the same number', () => {
+    expect(calculateAverage([5])).toBe(5);
+    expect(calculateAverage([0])).toBe(0);
+  });
+
+    test('empty list → should give null', () => {
+    expect(calculateAverage([])).toBe(null);
   });
