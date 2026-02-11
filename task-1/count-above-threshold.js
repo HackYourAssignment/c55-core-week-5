3bd5a60ece9ad24b0a3fc0bf9f705dbaa54e0af9
hannahwn <hannahwanjikunyongo@gmail.com>
2026-02-11 10:09:11 +0100

    gives total of numbers above threshold

--- task-1/count-above-threshold.js
+++ task-1/count-above-threshold.js
@@ -4,6 +4,12 @@ function countAboveThreshold(numbers, threshold) {
     let count = 0 ;
 
     for(let i = 0; i < numbers.length; i = i + 1){
-        
+
+        if(numbers[i] > threshold){
+
+            count = count + 1;
+        }
     }
+
+    return count;
 }
